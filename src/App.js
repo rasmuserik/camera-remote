3d17c625ad142bc5fbe580e2d5dca403c3a25b06
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2019-03-15 11:41:35 +0100

    ipfs experiment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
 
 class App extends Component {
   render() {
@@ -8,16 +7,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            blah blah
-          </p>
+          <p>blah blah</p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
-          </a>
+          />
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,28 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import {Provider} from 'react-redux';
-import {store} from './state';
+import { install as installStyles } from "@material-ui/styles";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { Provider } from "react-redux";
+import { store } from "./state";
+
+installStyles();
+
+const IPFS = require("ipfs");
+const node = new IPFS({
+  EXPERIMENTAL: { pubsub: true, dht: true },
+  config: {
+    Addresses: {
+      Swarm: [
+        "/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star"
+      ]
+    }
+  }
+});
+const id = Math.random()
+  .toString(36)
+  .slice(2);
+window.ipfs = node;
+
+node.on("ready", async () => {
+  try {
+    console.log("IPFS READY");
+    await node.pubsub.subscribe("solsort-stop-motion", msg =>
+      console.log("pubsub", new TextDecoder("utf-8").decode(msg.data))
+    );
+    setInterval(
+      () =>
+        node.pubsub.publish(
+          "solsort-stop-motion",
+          Buffer.from(`hello from ${id} ` + window.navigator.userAgent)
+        ),
+      5000
+    );
+  } catch (e) {
+    console.log("ipfserr", e);
+  }
+});
+node.on("error", error => {
+  console.error("IPFS Error:", error);
+});
 
 const theme = createMuiTheme({});
-
 function render() {
-ReactDOM.render(
-      <Provider store={store}>
-        <MuiThemeProvider theme={theme}>
-
-
+  ReactDOM.render(
+    <Provider store={store}>
+      <MuiThemeProvider theme={theme}>
         <App />
-</MuiThemeProvider>
-            </Provider> 
-        , document.getElementById('root'));
+      </MuiThemeProvider>
+    </Provider>,
+    document.getElementById("root")
+  );
 }
 render();
 
 if (module.hot) {
-    module.hot.accept(['./App'], render);
+  module.hot.accept(["./App"], render);
 }
 
 // If you want your app to work offline and load faster, you can change
