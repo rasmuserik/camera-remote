308f82ab65106cbfa1f61ebcc9b88531829e0d1e
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2019-03-15 10:34:37 +0100

    redux+mui-theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import {Provider} from 'react-redux';
+import {store} from './state';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({});
+
+function render() {
+ReactDOM.render(
+      <Provider store={store}>
+        <MuiThemeProvider theme={theme}>
+
+
+        <App />
+</MuiThemeProvider>
+            </Provider> 
+        , document.getElementById('root'));
+}
+render();
+
+if (module.hot) {
+    module.hot.accept(['./App'], render);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
