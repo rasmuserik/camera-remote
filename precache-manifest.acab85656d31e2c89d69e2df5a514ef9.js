a9c939581957bad9573d484ac3a7f3d09d5649df
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2019-03-15 10:04:43 +0100

    Updates

---
+++ precache-manifest.acab85656d31e2c89d69e2df5a514ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cad6a0f34bd4c5c4d637",
+    "url": "/static/js/main.f523431e.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "cad6a0f34bd4c5c4d637",
+    "url": "/static/css/main.97c68cf9.chunk.css"
+  },
+  {
+    "revision": "cfcd09b7edad72faa91af15e10eb06e8",
+    "url": "/index.html"
+  }
+];
